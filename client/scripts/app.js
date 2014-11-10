6e7f511aa7a9e1e49574c535dd449ea15377a354
grantbot <grantwu2@gmail.com>
2014-11-10 15:39:27 -0800

    message retrieval and display functions working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,50 @@
-// YOUR CODE HERE:
 
+var response;
+
+function getMessages() {
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    data: {
+      format:'json'
+    },
+    type: 'GET',
+    dataType: 'json',
+    success: function(data) {
+      response = data.results;
+      displayMessages(response);
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+  return response;
+}
+
+function displayMessages(messageArray) {
+  for (var i=0; i<messageArray.length; i++) {
+    var $newMessage = $('<li></li>');
+    $newMessage.html(messageArray[i].username + ' says: ' +
+      messageArray[i].text);
+    $('.messages').append($newMessage);
+  }
+}
+
+function refreshMessages() {
+  clearMessages();
+  var newMessages = getMessages();
+  displayMessages(newMessages);
+}
+
+function clearMessages() {
+  $('.messages').html('')
+}
+
+// - gary says: "hello"
+
+// createdAt: "2013-10-07T16:22:03.280Z"
+// objectId: "teDOY3Rnpe"
+// roomname: "lobby"
+// text: "hello"
+// updatedAt: "2013-10-07T16:22:03.280Z"
+// username: "gary"
